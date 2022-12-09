172a62dada59c94e25c9d55ab414e88f4c744b15
Peter Forret <peter@forret.com>
2022-12-09 14:01:51 +0100

    MOD: footer.blade.php

--- resources/views/_template/bootstrap/footer.blade.php
+++ resources/views/_template/bootstrap/footer.blade.php
@@ -4,7 +4,7 @@
 
 <footer class="text-muted py-5">
     <div class="container">
-        <p class="mb-1"><strong>Generationz</strong> v@include('VERSION') &copy; 2022 by Peter Forret</p>
+        <p class="mb-1"><strong>Generationz</strong> v @include('VERSION') &copy; 2022 by Peter Forret</p>
     </div>
 </footer>
 </body></html>
